3f9b537bd531eef0ec32664a02eea8d3d439c3fc
Annie Clinton <annelisabethclinton1@gmail.com>
2023-05-04 10:29:29 -0700

    solution

---
+++ fake-binary2.js
@@ -0,0 +1,12 @@
+function fakeBin(x){
+  let newStr = ''
+  for(let i = 0; i < x.length; i++){
+    if(x[i] < 5){
+      newStr += 0
+    }else{
+      newStr += 1
+    }
+  }
+  return newStr
+}
+console.log(fakeBin('444666'))
