294bbb1f6b84c2afd258cae6239e475f6b22aa40
Annie Clinton <annelisabethclinton1@gmail.com>
2023-12-13 12:50:33 -0800

    solution

---
+++ whats-up-next.js
@@ -0,0 +1,14 @@
+function nextItem(xs, item) {
+  let found = false;
+  for (let x of xs) {
+    if (found) return x;
+    if (x == item) found = true;
+  }
+  return undefined;
+}
+
+console.log(nextItem([1, 2, 3, 4, 5, 6, 7], 3)) //4
+console.log(nextItem("testing", "t")) // "e"
+
+
+ 
