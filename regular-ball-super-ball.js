3b6044f20d6bc5e96508bf1a39849020d268c416
Annie Clinton <annelisabethclinton1@gmail.com>
2023-12-15 10:47:55 -0800

    solution

---
+++ regular-ball-super-ball.js
@@ -0,0 +1,13 @@
+
+
+class Ball{
+  constructor(ballType="regular"){
+    this.ballType = ballType
+  }
+}
+
+let ball1 = new Ball()
+let ball2 = new Ball("super")
+
+console.log(ball1.ballType)
+console.log(ball2.ballType)
