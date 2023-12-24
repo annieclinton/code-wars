b66001956cdd388af32e59ff46c9f7aa2b35f7c8
Annie Clinton <annelisabethclinton1@gmail.com>
2023-12-24 13:07:24 -0800

    solution

---
+++ expressions-matter.js
@@ -0,0 +1,12 @@
+function expressionMatter(a, b, c) {
+  return Math.max(a + b + c, 
+    a + b * c,
+    (a + b) * c,
+    a * b * c,
+    a * b + c,
+    a * (b + c))
+}
+
+
+
+console.log(expressionMatter(1, 2, 3))
