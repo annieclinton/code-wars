caa80b1809eb8821f9797b1176dfc26b4764869c
Annie Clinton <annelisabethclinton1@gmail.com>
2024-01-09 13:53:56 -0800

    solution

---
+++ parse-nice-int-from-char-problem.js
@@ -0,0 +1,11 @@
+
+function getAge(inputString){
+  return +inputString[0]
+  }
+
+// function getAge(inputString){
+//   return parseInt(inputString)
+//   }
+
+
+  console.log(getAge("9 years old"))
