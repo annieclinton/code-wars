ae1eb1f2e3bc5074c59314fb04fb7e7f53d14f4f
Annie Clinton <annelisabethclinton1@gmail.com>
2024-01-13 20:25:18 -0800

    solution

---
+++ L1-set-alarm.js
@@ -0,0 +1,16 @@
+// function setAlarm(employed, vacation){
+//   if(employed === true && vacation === true){
+//     return false
+//   }else if(employed === true && vacation === false){
+//     return true
+//   }else if(employed === false && vacation === true){
+//     return false
+//   }else{
+//     return false
+//   }
+// }
+
+
+function setAlarm(employed, vacation){
+  return employed && !vacation ? true : false
+}
