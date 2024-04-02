e33ebb30784bb910d22b1b69e16418251b2e1e15
Annie Clinton <annelisabethclinton1@gmail.com>
2024-04-02 15:10:25 -0700

    solution

---
+++ find-max-and-min-values-in-a-list.js
@@ -0,0 +1,20 @@
+var min = function(list){
+    
+    return Math.min(...list)
+  }
+  
+  var max = function(list){
+    
+    return Math.max(...list)
+  }
+  
+  
+  
+  console.log(min([4,6,2,1,9,63,-134,566]))
+  console.log(max([4,6,2,1,9,63,-134,566]))
+  
+  let arr = [1,2,3,4]
+  console.log(...arr)
+  console.log(...[1,2,3,4])
+console.log(min([4,6,2,1,9,63,-134,566]))
+console.log(max([4,6,2,1,9,63,-134,566]))
