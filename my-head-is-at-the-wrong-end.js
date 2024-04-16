f55390e90d41f719e0d9291716548552d174eba5
Annie Clinton <annelisabethclinton1@gmail.com>
2024-04-16 14:47:33 -0700

    solution

---
+++ my-head-is-at-the-wrong-end.js
@@ -0,0 +1,17 @@
+
+
+// array with 3 values, swap value 1 and value 3
+//P: array, always strings, could include spaces
+//R: return same array, edited
+//E:
+
+function fixTheMeerkat(arr){
+ //take in arr
+ //swap index 0 and index 2
+ //return array
+	return arr.reverse()
+}
+
+console.log(fixTheMeerKat(["tail", "body", "head"],)) // ["head","body","tail"]
+console.log(fixTheMeerKat(["two feet", "the body", "one head"])) //["one head", "the body", "two feet"]
+console.log(fixTheMeerKat(["shoes", "pants", "hat"])) //["hat", "pants", "shoes"]
