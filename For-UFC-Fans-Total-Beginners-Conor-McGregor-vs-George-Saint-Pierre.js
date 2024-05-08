7e9c1d80e0d23def29665c82a6d624a42721daf8
Annie Clinton <annelisabethclinton1@gmail.com>
2024-05-08 10:29:51 -0700

    solution

---
+++ For-UFC-Fans-Total-Beginners-Conor-McGregor-vs-George-Saint-Pierre.js
@@ -0,0 +1,4 @@
+var quote = function(fighter) {
+  fighter = fighter.toLowerCase()
+  return fighter === "george saint pierre" ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+};
